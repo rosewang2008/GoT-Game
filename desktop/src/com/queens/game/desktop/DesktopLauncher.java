6f56a1d2101846f2b373ab12448ed8a65c0f3646
Aditi Srinivasan <aditisri@mit.edu>
2018-01-15 11:20:03 -0500

    [Setup] Initialized gdx project

---
+++ desktop/src/com/queens/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.queens.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.queens.game.QueensGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new QueensGame(), config);
+	}
+}
